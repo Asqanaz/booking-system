b87e809eb0ee2ffae6cee7ccdf044750fc2ccdf6
Asqanaz <asmoyan.04@gmail.com>
2023-09-29 16:49:18 +0400

    finalize

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,14 +2,14 @@ import AdminPage from "../pages/AdminPage"
 
 export const ADMIN_ROUTES = [
     {
-        path: 'services',
-        element: AdminPage,
-        title: 'Services'
-    },
-    {
         path: 'professionals',
         element: AdminPage,
         title: 'Professionals'
+    },
+    {
+        path: 'services',
+        element: AdminPage,
+        title: 'Services'
     },
     {
         path: 'available-hours',
